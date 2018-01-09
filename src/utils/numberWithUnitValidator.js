32fc8dc9b7044eee0c203f91768368891f818ef7
Pedr Browne <pedr.browne@gmail.com>
2018-01-09 15:29:48 +0000

    fix(numberWithUnitValidator): Remove array wrapping error message

--- src/utils/numberWithUnitValidator.js
+++ src/utils/numberWithUnitValidator.js
@@ -3,6 +3,7 @@ import predicateValidator from './predicateValidator';
 
 // Create a type validator
 export default unit =>
-  predicateValidator(isNumberWithUnit([unit]), [
-    `Wasn't number with unit: '${unit}'`,
-  ]);
+  predicateValidator(
+    isNumberWithUnit([unit]),
+    `Wasn't number with unit: '${unit}'`
+  );
