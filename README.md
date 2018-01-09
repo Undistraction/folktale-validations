f81bf93faa45812c071517bb215b1b8b59c7d58c
Pedr Browne <pedr.browne@gmail.com>
2018-01-09 12:37:33 +0000

    init(project): Basic Project Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# folktale-validations
+
+A library of validators using [folktale](http://folktale.origamitower.com/)'s `Validation` including utility functions for creating validators with less boilerplate and specialised validators.
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bail: true,
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [`src/**/*.js`],
+  coveragePathIgnorePatterns: [`src/index.js`],
+  coverageReporters: [`json`],
+  setupFiles: [],
+  modulePathIgnorePatterns: [
+    `helpers/`,
+    `sharedTests/`,
+    `data.js`,
+    `featureValues.js`,
+  ],
+};
