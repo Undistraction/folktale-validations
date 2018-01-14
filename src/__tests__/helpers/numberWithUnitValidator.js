a2c0658d244042d0f1e35b5520328bc57d2c93e0
Pedr Browne <pedr.browne@gmail.com>
2018-01-14 10:27:11 +0000

    test(numberWithUnitValidator): Add tests

---
+++ src/__tests__/helpers/numberWithUnitValidator.js
@@ -0,0 +1,66 @@
+import { validation as Validation } from 'folktale';
+import { map } from 'ramda';
+import { numberWithUnitValidator } from '../../index';
+import { numberWithUnitErrorMessage } from '../../messages';
+
+const { Success, Failure } = Validation;
+
+describe(`numberWithUnitValidator`, () => {
+  describe(`when value is valid`, () => {
+    it(`returns a Validation.Success with the supplied value`, () => {
+      const numbers = [0, 0.5, -0.5, 10, -10];
+      map(number => {
+        const unit = `xx`;
+        const value = `${number}${unit}`;
+        const validator = numberWithUnitValidator(unit);
+        const result = validator(value);
+        expect(Success.hasInstance(result)).toBeTruthy();
+        expect(result.value).toEqual(value);
+      })(numbers);
+    });
+  });
+
+  describe(`when value is invalid`, () => {
+    describe(`with unitless numbers`, () => {
+      it(`returns a Validation.Failure with message`, () => {
+        const numbers = [0, 0.5, -0.5, 10, -10];
+        map(number => {
+          const unit = `px`;
+          const value = `${number}`;
+          const validator = numberWithUnitValidator(unit);
+          const result = validator(value);
+          expect(Failure.hasInstance(result)).toBeTruthy();
+          expect(result.value).toEqual([numberWithUnitErrorMessage(unit)]);
+        })(numbers);
+      });
+    });
+
+    describe(`with united values`, () => {
+      it(`returns a Validation.Failure with message`, () => {
+        const numbers = [`0yy`, `0.5y`, `-0.5xy`];
+        map(number => {
+          const unit = `px`;
+          const value = `${number}`;
+          const validator = numberWithUnitValidator(unit);
+          const result = validator(value);
+          expect(Failure.hasInstance(result)).toBeTruthy();
+          expect(result.value).toEqual([numberWithUnitErrorMessage(unit)]);
+        })(numbers);
+      });
+    });
+
+    describe(`with other invalid values`, () => {
+      it(`returns a Validation.Failure with message`, () => {
+        const numbers = [[], {}, null, undefined, `s`, /x/, true, false];
+        map(number => {
+          const unit = `px`;
+          const value = `${number}`;
+          const validator = numberWithUnitValidator(unit);
+          const result = validator(value);
+          expect(Failure.hasInstance(result)).toBeTruthy();
+          expect(result.value).toEqual([numberWithUnitErrorMessage(unit)]);
+        })(numbers);
+      });
+    });
+  });
+});
