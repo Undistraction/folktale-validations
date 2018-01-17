fc7f7515bd9f2f45f54667c1d6abbbd2d255c663
Pedr Browne <pedr.browne@gmail.com>
2018-01-17 00:48:43 +0000

    feat(logging): Add logging functions to utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { join, compose, map } from 'ramda';
+import { join, compose, map, append, flip, curry, tap } from 'ramda';
 
 // eslint-disable-next-line import/prefer-default-export
 export const joinWithComma = join(`, `);
@@ -7,3 +7,12 @@ export const joinWithOr = join(` or `);
 export const joinWitColon = join(`: `);
 export const quote = value => `'${value}'`;
 export const quoteAndJoinWithComma = compose(joinWithComma, map(quote));
+
+const log = curry((loggingFunction, prefix) =>
+  tap(
+    compose(loggingFunction, join(`: `), flip(append)([prefix]), JSON.stringify)
+  )
+);
+
+// eslint-disable-next-line no-console
+export const logToConsole = log(console.log);
