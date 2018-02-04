0b08b101c25ed24b3b1eeb8030bd2d84b08efc55
Pedr Browne <pedr.browne@gmail.com>
2018-02-04 19:55:20 +0000

    tests(jasmine-matchers): Add lib of matchers

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,5 @@ module.exports = {
     `featureValues.js`,
     `testHelpers/`,
   ],
-};
+  unmockedModulePathPatterns: [`jasmine-expect`],
+}
--- src/__tests__/testHelpers/matchers/customMatchers.js
+++ src/__tests__/testHelpers/matchers/customMatchers.js
@@ -1,11 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import JasmineExpect from 'jasmine-expect'
 import toEqualWithCompressedWhitespace from './toEqualWithCompressedWhitespace'
 import toEqualSuccessWithValue from './toEqualSuccessWithValue'
 import toEqualFailureWithValue from './toEqualFailureWithValue'
-import toBeTrue from './toBeTrue'
 
 expect.extend({
   toEqualWithCompressedWhitespace,
   toEqualSuccessWithValue,
   toEqualFailureWithValue,
-  toBeTrue,
 })
