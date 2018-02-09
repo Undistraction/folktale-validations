e3c953123bb8b5f07e48df82bfc61ecc39228a78
Pedr Browne <pedr.browne@gmail.com>
2018-02-09 11:01:31 +0000

    refactor(failures utils): Clean up and add utils

--- src/failures/utils.js
+++ src/failures/utils.js
@@ -9,8 +9,6 @@ import {
   map,
   has,
   either,
-  __,
-  gt,
   both,
   all,
 } from 'ramda'
@@ -18,6 +16,8 @@ import { isObj } from 'ramda-adjunct'
 import { FAILURE_FIELD_NAMES } from '../const'
 import { propValue } from '../utils/failures'
 import { constraintsObjName } from '../messages'
+import { joinWithDot } from '../utils/formatting'
+import { appendRight } from '../utils/array'
 
 const {
   FIELDS_FAILURE_MESSAGE,
@@ -28,27 +28,21 @@ const {
   OR,
 } = FAILURE_FIELD_NAMES
 
+const UIDPrefix = `folktale-validations`
+
 export const toObjectError = compose(objOf(FIELDS), mergeAll, fromPairs)
-
-export const toObjectFieldsError = objOf(FIELDS_FAILURE_MESSAGE)
-
-export const addObjectName = assoc(NAME)
-
 export const toConstraintsError = compose(assoc(NAME, constraintsObjName()))
-
+export const toObjectFieldsError = objOf(FIELDS_FAILURE_MESSAGE)
 export const toArrayError = objOf(CHILDREN, of)
-
 const failuresToChildren = map(compose(objOf(CHILDREN), map(propValue)))
-
 export const toChildrenFieldsError = compose(objOf(FIELDS), failuresToChildren)
 
+export const addObjectName = assoc(NAME)
 export const propAnd = prop(AND)
-
 export const propOr = prop(OR)
-
 export const isAndObj = has(AND)
 export const isOrObj = has(OR)
 export const isAndOrOrObj = both(isObj, either(isAndObj, isOrObj))
 export const isPayload = all(has())
 
-export const greaterThanZero = gt(__, 0)
+export const toUID = compose(joinWithDot, appendRight([UIDPrefix]))
