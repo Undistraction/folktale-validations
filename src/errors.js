d4be5bc0027a4d614fbc03028e86946b22e49000
Pedr Browne <pedr.browne@gmail.com>
2018-02-19 19:02:43 +0100

    refactor(errors): Consistant error messages

--- src/errors.js
+++ src/errors.js
@@ -1,25 +1,41 @@
 import { map, compose, toPairs } from 'ramda'
-import { joinWithColon, joinWithComma } from './utils/formatting'
+import {
+  joinWithColon,
+  joinWithComma,
+  wrapWithSquareBrackets,
+  joinWithSpace,
+} from './utils/formatting'
+
+const validatorPrefix = wrapWithSquareBrackets(`renderer`)
 
 export const throwError = message => {
   throw new Error(message)
 }
 
 export const noMessageForKeyErrorMessage = uid =>
-  `[validator message map] No message for validator UID '${uid}`
+  joinWithSpace([
+    validatorPrefix,
+    `Can't render message. No message for validator UID '${uid}'`,
+  ])
 
 export const messageValueMustBeFunction = kvPairs =>
-  `[validator message map] All values must be functions but You supplied an object with invalid values: ${compose(
-    joinWithComma,
-    map(joinWithColon),
-    toPairs,
-    map(toString)
-  )(kvPairs)}`
+  joinWithSpace([
+    validatorPrefix,
+    `All messages must be functions but You supplied an object with invalid values: ${compose(
+      joinWithComma,
+      map(joinWithColon),
+      toPairs,
+      map(toString)
+    )(kvPairs)}`,
+  ])
 
 export const invalidFailureStructureErrorMessage = value =>
-  `[default renderer] Couldn't render failure. Structure not recognised: ${JSON.stringify(
-    value
-  )}`
+  joinWithSpace([
+    validatorPrefix,
+    `Couldn't render failure. Structure not recognised: ${JSON.stringify(
+      value
+    )}`,
+  ])
 
 export const throwInvalidFailureStructureMessage = compose(
   throwError,
