a34f76a5602ef1cb6a942b04bf87e97696ffc439
Pedr Browne <pedr.browne@gmail.com>
2018-03-11 15:45:19 +0000

    feat: Add support for RegExp in field names

--- src/utils/function.js
+++ src/utils/function.js
@@ -1,4 +1,37 @@
-import { flip, apply } from 'ramda'
+import {
+  sort,
+  comparator,
+  prop,
+  pipe,
+  flip,
+  apply,
+  head,
+  curryN,
+  reduce,
+  reduced,
+  curry,
+  ifElse,
+} from 'ramda'
+import { isNotNil, isNonEmptyArray, stubUndefined } from 'ramda-adjunct'
 
 // eslint-disable-next-line import/prefer-default-export
 export const applyFlipped = flip(apply)
+
+const byArity = comparator((a, b) => a.length > b.length)
+
+const getMaxArity = pipe(sort(byArity), head, prop(`length`))
+
+const iteratorFn = curry((args, accumulator, fn) => {
+  const result = fn(...args)
+
+  return isNotNil(result) ? reduced(result) : accumulator
+})
+
+const dispatchImp = functions => {
+  const arity = getMaxArity(functions)
+  return curryN(arity, (...args) =>
+    reduce(iteratorFn(args), undefined, functions)
+  )
+}
+
+export const dispatch = ifElse(isNonEmptyArray, dispatchImp, stubUndefined)
--- src/validators/object/validateObjectValues.js
+++ src/validators/object/validateObjectValues.js
@@ -1,24 +1,33 @@
 import { validation as Validation } from 'folktale'
 import {
   applyTo,
-  ifElse,
   prop,
   compose,
   reduce,
   toPairs,
   always,
   pair,
+  filter,
+  keys,
+  when,
   of,
+  test,
   tryCatch,
+  flip,
+  find,
+  slice,
+  ifElse,
 } from 'ramda'
-import { isNotUndefined } from 'ramda-adjunct'
+import { isNotUndefined, isNotEmpty, stubUndefined } from 'ramda-adjunct'
 import { toObjectError } from '../../utils/failures'
+import { dispatch } from '../../utils/function'
 import {
   propValue,
   matchWithSuccessOrFailure,
   successOrElse,
 } from '../../utils/validations'
 import { throwError, validatorError } from '../../errors'
+import { STRING_REGEXP_MATCH } from '../../constraints/const'
 
 const { Success, Failure } = Validation
 
@@ -30,14 +39,31 @@ const validateValue = (acc, name, value) =>
     tryCatch(applyTo(value), _ => throwError(validatorError(name, value)))
   )
 
-const validateExplicitFields = validatorsMap => (acc, [name, value]) =>
+const filterRegExpValidators = compose(filter(test(STRING_REGEXP_MATCH)), keys)
+
+const testName = name => compose(flip(test)(name), RegExp, slice(1, -1))
+
+const validatorFromRegExName = name => validatorsMap =>
+  compose(
+    ifElse(
+      isNotEmpty,
+      compose(
+        when(isNotUndefined, flip(prop)(validatorsMap)),
+        find(testName(name))
+      ),
+      stubUndefined
+    ),
+    filterRegExpValidators
+  )(validatorsMap)
+
+const valueReducer = validatorsMap => (acc, [name, value]) =>
   compose(
     ifElse(isNotUndefined, validateValue(acc, name, value), always(acc)),
-    prop(name)
+    dispatch([prop(name), validatorFromRegExName(name)])
   )(validatorsMap)
 
 const validateValues = (validatorsMap, o) =>
-  reduce(validateExplicitFields(validatorsMap), Success(o))
+  reduce(valueReducer(validatorsMap), Success(o))
 
 const validateObjectValues = validatorsMap => o =>
   compose(
