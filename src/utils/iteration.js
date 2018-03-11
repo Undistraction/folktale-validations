8906f8f35d2442c4c047a20ad113d0b307d81734
Pedr Browne <pedr.browne@gmail.com>
2018-03-11 12:11:41 +0000

    refactor: Remove unused util functions

--- src/utils/iteration.js
+++ src/utils/iteration.js
@@ -6,10 +6,3 @@ export const reduceObjIndexed = curry((f, acc, v) =>
 
 export const mapObjIndexedWithIndex = addIndex(mapObjIndexed)
 export const reduceObjIndexedWithIndex = addIndex(reduceObjIndexed)
-
-const predicateIterator = (predicate, iterator) => (acc, v) =>
-  predicate(v) ? iterator(acc, v) : acc
-
-export const reduceIf = curry((predicate, iterator, acc, v) =>
-  reduce(predicateIterator(predicate, iterator), acc, v)
-)
