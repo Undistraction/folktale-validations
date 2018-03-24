d6ea125df8e715fcd3d8c5b351c1c1a867c9e783
Pedr Browne <pedr.browne@gmail.com>
2018-03-24 17:43:10 +0000

    refactor(fromArgsObj): Lean on ramda

--- src/utils/args.js
+++ src/utils/args.js
@@ -1,6 +1,5 @@
-import { compose, converge, map, prop } from 'ramda'
-import { list } from 'ramda-adjunct'
+import { props } from 'ramda'
 import { pickDefined } from './object'
 
 export const toArgsObj = pickDefined
-export const fromArgsObj = compose(converge(list), map(prop))
+export const fromArgsObj = props
